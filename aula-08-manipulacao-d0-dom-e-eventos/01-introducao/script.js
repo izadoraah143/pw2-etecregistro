8d0b40cbf170fb84295980a6e649ac5aa1b9a542
IZADORA DE OLIVEIRA <izadora.oliveira@e239.local>
2026-08-24 09:00:12 -0300

    aula 08

---
+++ aula-08-manipulacao-d0-dom-e-eventos/01-introducao/script.js
@@ -0,0 +1,47 @@
+// JavaScript
+
+// Capturando um elemento do DOM
+// Não é recomendado usá-lo para estilização, e sim para interação
+
+const titulo = window.document.getElementsByTagName('h1')[0] 
+// Foi pela árvore dom desde a tag pai (window) e selecionou o 1º elemento h1 = [0]
+
+titulo.innerText = "Fui alterado pelo JavaScript TT" 
+// Altera o conteúdo do título
+// Poderia ser usado para alterar o título quando o usuário fizer detyerminada coisa
+
+titulo.style.color = 'yellow'
+// Altera a cor do título
+
+// tags de 1º nível como body
+ const pagina = window.document.body
+ pagina.style.backgroundColor = '#f88ce1'
+
+ // Eventos
+// Capturando a div 
+const caixaMagica = window.document.getElementById('caixaMagica');
+
+// Organixar vários eventos em funções
+
+function entrada() {
+    caixaMagica.innerText = 'Oi! :)'
+    caixaMagica.style.backgroundColor = 'blue'
+}
+
+function saida(){
+    caixaMagica.innerText = 'Tchau! :D'
+    caixaMagica.style.backgroundColor = 'black'
+}
+
+function clicar(){
+    caixaMagica.innerText = 'Clicou!'
+    caixaMagica.style.backgroundColor = 'red'
+}
+
+// Escutando um evento só da caixa
+// adicione um escutador de evento na caixa
+caixaMagica.addEventListener ('mouseenter', entrada)
+caixaMagica.addEventListener('mouseout', saida)
+caixaMagica.addEventListener('click', clicar)
+
+
