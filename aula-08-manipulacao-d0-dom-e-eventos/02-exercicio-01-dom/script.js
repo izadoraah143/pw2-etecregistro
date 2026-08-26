8baae9edacb1b594ed281e3d62727164eab30999
Izadora de Oliveira <ysaisa41@gmail.com>
2026-08-25 22:52:13 -0300

    exercicio-02-dom

--- aula-08-manipulacao-d0-dom-e-eventos/02-exercicio-01-dom/script.js
+++ aula-08-manipulacao-d0-dom-e-eventos/02-exercicio-01-dom/script.js
@@ -1,1 +1,31 @@
-const 
+const yellow = window.document.getElementById('yellow');
+const green = window.document.getElementById('green');
+const red = window.document.getElementById('red');
+
+const parar = window.document.getElementById('parar')
+const atencao = window.document.getElementById('atencao')
+const prosseguir = window.document.getElementById('prosseguir')
+
+function clicarParar() {
+    red.style.backgroundColor = '#ff0000'
+    yellow.style.backgroundColor = 'gray'
+    green.style.backgroundColor = 'gray'
+}
+
+function clicarAtencao() {
+    yellow.style.backgroundColor = '#ffee00'
+    green.style.backgroundColor = 'gray'
+    red.style.backgroundColor = 'gray'
+}
+
+function clicarProsseguir() {
+    green.style.backgroundColor = '#00fd15'
+    red.style.backgroundColor = 'gray'
+    yellow.style.backgroundColor = 'gray'
+}
+
+parar.addEventListener ('click', clicarParar)
+atencao.addEventListener ('click', clicarAtencao)
+prosseguir.addEventListener ('click', clicarProsseguir)
+
+
